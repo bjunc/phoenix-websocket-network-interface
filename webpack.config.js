935769a7a00bb9c55280b1f727f6587f8a96597d
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-10-15 22:52:53 -0400

    targeting node to ensure webpack doesn’t grab websocket’s browser only script

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+    target: 'node',
     entry: './src/phoenix-websocket-network-interface.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
